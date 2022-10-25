f690f493e31b076318bf3b2ddbef690ad869b9b4
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-10-25 22:37:42 +0600

    added the server side and courses

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
+const courses = require('./data/courses.json')
+
 app.get('/', (req, res)=>{
     res.send('learning API Runing');
 });
+
+app.get('/regular-courses', (req, res)=>{
+    res.send(courses)
+})
 
 app.listen(port, ()=>{
     console.log('learning server side', port);
